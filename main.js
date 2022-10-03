b039bd5304ec57d47a9952bd2f7225ae017e3161
Jiyoung <jssq2468@naver.com>
2022-10-03 20:39:21 +0900

    Fix: css 스타일 수정

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-"use strict";
+'use strict';
 
 // navbar animation
 
-const navbar = document.querySelector("#navbar");
-const navbarLogo = document.querySelector(".navbar__logo__name");
-const navbarBtn = document.querySelector(".navbar__toggle-btn");
+const navbar = document.querySelector('#navbar');
+const navbarLogo = document.querySelector('.navbar__logo__name');
+const navbarBtn = document.querySelector('.navbar__toggle-btn');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
-    navbarLogo.classList.add("navbar--dark");
-    navbarBtn.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
+    navbarLogo.classList.add('navbar--dark');
+    navbarBtn.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark");
-    navbarLogo.classList.remove("navbar--dark");
-    navbarBtn.classList.remove("navbar--dark");
+    navbar.classList.remove('navbar--dark');
+    navbarLogo.classList.remove('navbar--dark');
+    navbarBtn.classList.remove('navbar--dark');
   }
 });
 
 // navbar tap scroll , navbar click css
 
-const navbarMenu = document.querySelector(".navbar__menu");
+const navbarMenu = document.querySelector('.navbar__menu');
 
-navbarMenu.addEventListener("click", (event) => {
+navbarMenu.addEventListener('click', event => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
@@ -33,22 +33,22 @@ navbarMenu.addEventListener("click", (event) => {
 
 //contact btn scroll
 
-const contactBtn = document.querySelector(".home__contact");
-contactBtn.addEventListener("click", () => {
-  scrollIntoView("#contact");
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
 });
 
 // scroll 투명도
 
-const home = document.querySelector(".home__container");
-const about = document.querySelector(".about__container");
-const skill = document.querySelector(".skill__container");
-const project = document.querySelector(".project__container");
+const home = document.querySelector('.home__container');
+const about = document.querySelector('.about__container');
+const skill = document.querySelector('.skill__container');
+const project = document.querySelector('.project__container');
 
 adjustOpacity(home);
 
 function adjustOpacity(element) {
-  document.addEventListener("scroll", () => {
+  document.addEventListener('scroll', () => {
     const offsetBottom = element.offsetTop + element.offsetHeight;
     if (window.scrollY < offsetBottom && window.scrollY > element.offsetTop) {
       element.style.opacity = element.offsetTop / window.scrollY;
@@ -59,19 +59,19 @@ function adjustOpacity(element) {
 }
 
 // backbtn scroll event & navbar toggle envent remove
-const backBtn = document.querySelector(".backBtn");
-backBtn.addEventListener("click", () => {
-  scrollIntoView("#home");
+const backBtn = document.querySelector('.backBtn');
+backBtn.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   const homeHeight = home.getBoundingClientRect().height;
   if (window.scrollY > homeHeight / 2) {
-    backBtn.classList.add("active");
+    backBtn.classList.add('active');
   } else {
-    backBtn.classList.remove("active");
+    backBtn.classList.remove('active');
   }
 
-  navbarMenu.classList.remove("open");
+  navbarMenu.classList.remove('open');
 });
 
 // skill animation
@@ -82,15 +82,15 @@ function gageStyle(element) {
   element.style.opacity = 1;
 }
 
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   const skillHeight = skill.getBoundingClientRect().height;
 
-  const skillGageHtml = document.querySelector(".skill__bar__gauge.html");
-  const skillGageCss = document.querySelector(".skill__bar__gauge.css");
-  const skillGageJs = document.querySelector(".skill__bar__gauge.javascript");
-  const skillGageReact = document.querySelector(".skill__bar__gauge.react");
-  const skillGageNode = document.querySelector(".skill__bar__gauge.node");
-  const skillGageBoot = document.querySelector(".skill__bar__gauge.boot");
+  const skillGageHtml = document.querySelector('.skill__bar__gauge.html');
+  const skillGageCss = document.querySelector('.skill__bar__gauge.css');
+  const skillGageJs = document.querySelector('.skill__bar__gauge.javascript');
+  const skillGageReact = document.querySelector('.skill__bar__gauge.react');
+  const skillGageNode = document.querySelector('.skill__bar__gauge.node');
+  const skillGageBoot = document.querySelector('.skill__bar__gauge.boot');
   if (window.scrollY > skillHeight) {
     gageStyle(skillGageReact);
     gageStyle(skillGageHtml);
@@ -110,38 +110,36 @@ document.addEventListener("scroll", () => {
 
 // max-width 768px toggle button
 
-const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
-navbarToggleBtn.addEventListener("click", () => {
-  navbarMenu.classList.toggle("open");
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 //nav button scroll  활성화
 
-const sectionIds = ["#home", "#about", "#skill", "#project", "#contact"];
-const sections = sectionIds.map((id) => document.querySelector(id));
-const navItems = sectionIds.map((id) =>
-  document.querySelector(`[data-link="${id}"]`)
-);
+const sectionIds = ['#home', '#about', '#skill', '#project', '#contact'];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   selectNavItem(navItems[selectedNavIndex]);
 });
 
 function selectNavItem(selected) {
-  selectedNavItem.classList.remove("select");
+  selectedNavItem.classList.remove('select');
   selectedNavItem = selected;
-  selectedNavItem.classList.add("select");
+  selectedNavItem.classList.add('select');
 }
 let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
 const observerOptions = {
   root: null,
-  rootMargin: "0px",
+  rootMargin: '0px',
   threshold: 0.3,
 };
 
 const obserCallback = (entries, observer) => {
-  entries.forEach((entry) => {
+  entries.forEach(entry => {
     if (!entry.isIntersecting && entry.intersectionRatio > 0) {
       const index = sectionIds.indexOf(`#${entry.target.id}`);
       if (entry.boundingClientRect.y < 0) {
@@ -153,15 +151,12 @@ const obserCallback = (entries, observer) => {
   });
 };
 const observer = new IntersectionObserver(obserCallback, observerOptions);
-sections.forEach((section) => observer.observe(section));
+sections.forEach(section => observer.observe(section));
 
-window.addEventListener("scroll", () => {
+window.addEventListener('wheel', () => {
   if (window.scrollY === 0) {
     selectedNavIndex = 0;
-  } else if (
-    window.scrollY + window.innerHeight ===
-    document.body.scrollHeight
-  ) {
+  } else if (window.scrollY + window.innerHeight === document.body.scrollHeight) {
     selectedNavIndex = navItems.length - 1;
   }
   selectNavItem(navItems[selectedNavIndex]);
@@ -169,6 +164,6 @@ window.addEventListener("scroll", () => {
 
 function scrollIntoView(selector) {
   const scroll = document.querySelector(selector);
-  scroll.scrollIntoView({ behavior: "smooth" });
+  scroll.scrollIntoView({ behavior: 'smooth' });
   selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
