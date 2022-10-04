3711454489327869a22075f6b2ef137da8ee3007
Jiyoung <jssq2468@naver.com>
2022-10-04 14:59:48 +0900

    CSS : 반응형 추가

--- main.js
+++ main.js
@@ -1,208 +1,169 @@
 'use strict';
-//Navbar
+
+// navbar animation
+
 const navbar = document.querySelector('#navbar');
-
-
+const navbarLogo = document.querySelector('.navbar__logo__name');
+const navbarBtn = document.querySelector('.navbar__toggle-btn');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
-
-document.addEventListener('scroll',()=>{
-
-if(window.scrollY>navbarHeight){
-    navbar.classList.add('navbar--dark') ;
-}else{
-    navbar.classList.remove('navbar--dark') ;
-}
-
-});
-//scroll
-const navbarMenu= document.querySelector('.navbar__menu');
-
-navbarMenu.addEventListener('click',(event)=>{
-  const link =event. target .dataset.link;
-    if(link==null){
-        return;
-    }
-    scrollIntoView(link);
-  
-   
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+    navbarLogo.classList.add('navbar--dark');
+    navbarBtn.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+    navbarLogo.classList.remove('navbar--dark');
+    navbarBtn.classList.remove('navbar--dark');
+  }
 });
 
-//hamburger button for small screen
-const navbarToggleBtn= document.querySelector('.navbar__toggle-btn');
+// navbar tap scroll , navbar click css
 
-navbarToggleBtn.addEventListener('click', () => {
-navbarMenu.classList.toggle('open');
+const navbarMenu = document.querySelector('.navbar__menu');
 
-
-});
-
-
-//contact
-
-const contact = document.querySelector('.home__contact');
-
-contact.addEventListener('click',() =>{
-
-    scrollIntoView('#contact');
-});
-
-//scoll slowly transparent
-
-const home= document.querySelector('#home');
-const homeheight = home.getBoundingClientRect().height;
-document.addEventListener('scroll',()=>{
-
-    home.style.opacity=1-window.scrollY/homeheight
-
-});
-
-
-//arrow up
-
-const arrowup=document.querySelector('.arrow-up');
-
-document.addEventListener('scroll', ()=>{
-
-    if(window.scrollY>homeheight/2){
-        arrowup.classList.add('visible');
-    }else{
-        arrowup.classList.remove('visible');
-
-    }
-
-});
-
-//click on the "arrow up"
-
-arrowup.addEventListener('click', ()=>{
-
-    scrollIntoView('#home');
-})
-
-
-
-//project filtering
-
-const workBtn = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-
-
-workBtn.addEventListener('click',(event)=>{
-const filter = event.target.dataset.filter ||  e.target.parentNode.dataset.filter;
-/* console.log(filter)를 하게되면 span버튼을 클릭했을때 undefined 이 나오기 때문에 
-html에서 각 span data-type 지정해주면 문제가 해결된다*/
-  if (filter == null) {
+navbarMenu.addEventListener('click', event => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
     return;
   }
-
-//botton state
-const active = document.querySelector('.category__btn.selected');
-active.classList.remove('selected');
-const target = event.target.nodeName ==='BUTTON' ? event.target : event.target.parentNode;
-target.classList.add('selected');
-
-
-//project animation
-projectContainer.classList.add('anim-out');
-
-setTimeout(()=>{
-    projectContainer.classList.remove('anim-out')
-},300);
-//0.3초뒤에 anim-out이 제거됨
-
-
-projects.forEach((pro)=>{
- 
-if(filter===pro.dataset.type ||filter==='all'){
-    pro.classList.remove('invisible');
-}else{
-    pro.classList.add('invisible');
-}
-
+  scrollIntoView(link);
 });
 
+//contact btn scroll
 
-
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
 });
 
-function scrollIntoView(selector){
-    const scrollTo=document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior : "smooth"});
+// scroll 투명도
 
+const home = document.querySelector('.home__container');
+const about = document.querySelector('.about__container');
+const skill = document.querySelector('.skill__container');
+const project = document.querySelector('.project__container');
+
+adjustOpacity(home);
+
+function adjustOpacity(element) {
+  document.addEventListener('scroll', () => {
+    const offsetBottom = element.offsetTop + element.offsetHeight;
+    if (window.scrollY < offsetBottom && window.scrollY > element.offsetTop) {
+      element.style.opacity = element.offsetTop / window.scrollY;
+    } else {
+      element.style.opacity = 1;
+    }
+  });
 }
 
+// backbtn scroll event & navbar toggle envent remove
+const backBtn = document.querySelector('.backBtn');
+backBtn.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+document.addEventListener('scroll', () => {
+  const homeHeight = home.getBoundingClientRect().height;
+  if (window.scrollY > homeHeight / 2) {
+    backBtn.classList.add('active');
+  } else {
+    backBtn.classList.remove('active');
+  }
 
-// loading
-const loader = document.querySelector('.loader');
-const html = document.querySelector('html');
+  navbarMenu.classList.remove('open');
+});
 
-html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
+// skill animation
+function gageNone(element) {
+  element.style.opacity = 0;
+}
+function gageStyle(element) {
+  element.style.opacity = 1;
+}
 
-window.addEventListener('load', ()=>{
+document.addEventListener('scroll', () => {
+  const skillHeight = skill.getBoundingClientRect().height;
 
-     setTimeout(() => { //로딩속도 구현
-         loader.style.opacity = '0';
-         html.style.overflow = 'auto'; //스크롤 방지 해제
-        
-        setTimeout(() => {
-         loader.style.display = 'none';
-      }, 800);
-  }, 1000);
-})
+  const skillGageHtml = document.querySelector('.skill__bar__gauge.html');
+  const skillGageCss = document.querySelector('.skill__bar__gauge.css');
+  const skillGageJs = document.querySelector('.skill__bar__gauge.javascript');
+  const skillGageReact = document.querySelector('.skill__bar__gauge.react');
+  const skillGageNode = document.querySelector('.skill__bar__gauge.node');
+  const skillGageBoot = document.querySelector('.skill__bar__gauge.boot');
+  if (window.scrollY > skillHeight) {
+    gageStyle(skillGageReact);
+    gageStyle(skillGageHtml);
+    gageStyle(skillGageCss);
+    gageStyle(skillGageJs);
+    gageStyle(skillGageNode);
+    gageStyle(skillGageBoot);
+  } else {
+    gageNone(skillGageReact);
+    gageNone(skillGageHtml);
+    gageNone(skillGageCss);
+    gageNone(skillGageJs);
+    gageNone(skillGageNode);
+    gageNone(skillGageBoot);
+  }
+});
 
-//1.모든 섹션 요소들과 해당메뉴아이템들을 가지고 온다
-//2. 인터섹션 옵저버를 이용해 모든 섹션을 관찰한다
-//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+// max-width 768px toggle button
 
-const sectionIds = [
-    '#home',
-    '#about',
-    '#skills',
-    '#work',
-    '#testimonials',
-    '#contact'
-];
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+//nav button scroll  활성화
+
+const sectionIds = ['#home', '#about', '#skill', '#project', '#contact'];
 const sections = sectionIds.map(id => document.querySelector(id));
-const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`))
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
 
-let selectedNavIndex=0;
+window.addEventListener('load', () => {
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('select');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('select');
+}
+let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
-function selectNavItem (selected){
-    // const navItem = navItems[selectedIndex]
-    // navItem.classList.add('active');
-    selectedNavItem.classList.remove('active');
-    selectedNavItem = selected;
-    selectedNavItem.classList.add('active');
-}
 const observerOptions = {
-    root:null,
-    rootMargin:'0px',
-    threshold:0.4,
-}
-const observerCallback = (entries,observer) =>{
-    entries.forEach(entry=>{
-        if(!entry.isIntersecting && entry.intersectionRatio>0){
-            const index = sectionIds.indexOf(`#${entry.target.id}`)
-            if(entry.boundingClientRect.y<0){
-                selectedNavIndex = index +1;             
-            }else{
-                selectedNavIndex = index -1;
-            } 
-        }  
-    })
-}
-const observer = new IntersectionObserver(observerCallback,observerOptions);
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.4,
+};
+
+const obserCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+const observer = new IntersectionObserver(obserCallback, observerOptions);
 sections.forEach(section => observer.observe(section));
 
-window.addEventListener('scroll',()=>{
-    if(window.scrollY ===0 ){
-        selectedNavIndex =0;
-    }else if (
-        window.scrollY + window.innerHeight === 
-        document.body.clientHeight){
-        selectedNavIndex = navItems.length -1;
-    }
-    selectNavItem(navItems[selectedNavIndex]);
-})
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (window.scrollY + window.innerHeight === document.body.scrollHeight) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+function scrollIntoView(selector) {
+  const scroll = document.querySelector(selector);
+  scroll.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
